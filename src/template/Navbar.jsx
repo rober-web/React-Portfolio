bc254925a0ba8d3129e19dc06790ea772b4a6fb9
rober-web <robp.medeiros@gmail.com>
2024-03-02 22:55:13 +0000

    added content to navbar

--- src/template/Navbar.jsx
+++ src/template/Navbar.jsx
@@ -1,7 +1,54 @@
 const Navbar = () => {
   return (
     <>
-      <h1>This is navbar</h1>
+      <nav className="navbar ">
+        <div className="container-fluid">
+          <a href="#" className="navbar-brand">
+            RoberWeb
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse  text-center "
+            id="navbarNavAltMarkup"
+          >
+            <div className="navbar-nav">
+              <a
+                href=""
+                className="nav-link active"
+                aria-current="page"
+                href="#"
+              >
+                Home
+              </a>
+            </div>
+            <div className="navbar-nav">
+              <a href="" className="nav-link" aria-current="page" href="#">
+                Gallery
+              </a>
+            </div>
+            <div className="navbar-nav">
+              <a href="" className="nav-link" aria-current="page" href="#">
+                About
+              </a>
+            </div>
+            <div className="navbar-nav">
+              <a href="" className="nav-link" aria-current="page" href="#">
+                Contact
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
