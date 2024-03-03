82873670ae85e490cf9ca1e4a75b2f0da1f6b6d4
rober-web <robp.medeiros@gmail.com>
2024-03-03 03:25:41 +0000

    installed react router and created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
 //import { useState } from "react";
 
 //import "./App.css";
-import Hero from "./components/hero/Hero";
+
 import Header from "./template/Header";
+import Footer from "./template/Footer";
+import HomePage from "./pages/HomePage";
+import GalleryPage from "./pages/GalleryPage";
+import AboutPage from "./pages/AboutPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <h1>404 Not Found</h1>,
+  },
+  {
+    path: "/home",
+    element: <HomePage />,
+    errorElement: <h1>404 Not Found</h1>,
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
+    errorElement: <h1>404 Not Found</h1>,
+  },
+  {
+    path: "/gallery",
+    element: <GalleryPage />,
+    errorElement: <h1>404 Not Found</h1>,
+  },
+]);
 
 const App = () => {
   return (
     <>
       <Header />
-      <Hero />
+      <RouterProvider router={router} />
+      <Footer />
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 //import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+   <App /> 
   </React.StrictMode>
 );
