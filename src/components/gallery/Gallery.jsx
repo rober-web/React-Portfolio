b857a98b37f88357fa8beb4173d6cacac99acdb8
rober-web <robp.medeiros@gmail.com>
2024-03-03 22:43:42 +0000

    added section and id for gallery

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -2,7 +2,8 @@ const Gallery = () => {
   return (
     <>
       <section id="gallery" className="animate__animated animate__fadeInRight">
-        <h1>this is the gallery</h1>
+        <h1 className="main-section-title text-center">Gallery</h1>
+        
       </section>
     </>
   );
