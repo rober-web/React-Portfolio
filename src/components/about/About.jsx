6b8a194c3e01b32eebde187073f357e9305b6e6c
rober-web <robp.medeiros@gmail.com>
2024-03-03 22:39:52 +0000

    edited about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,21 @@
 const About = () => {
   return (
-    <>
-      <h1>This is the about component</h1>
-    </>
+    <section
+      id="about"
+      className="min-vh-100 p-3 d-flex  align-items-center justify-content-center animate__animated animate__fadeInRight"
+    >
+      <div className="text-center gap-20">
+        <h1 className="main-section-title">About Me</h1>
+        <p className="main-section-text">
+          I am a passionate web developer dedicated to creating visually
+          stunning and seamlessly functional websites. With a keen eye for
+          design and a love for coding, I strive to bring your digital vision to
+          life. Let's collaborate to build a compelling online presence that
+          leaves a lasting impression.
+        </p>
+      </div>
+    </section>
   );
 };
+
 export default About;
