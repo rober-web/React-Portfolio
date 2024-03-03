dae090df736939b04b6f2b9098eb26a18a7edab6
rober-web <robp.medeiros@gmail.com>
2024-03-03 23:10:12 +0000

    added content and style to Social

---
+++ src/components/social/Social.jsx
@@ -0,0 +1,13 @@
+const Social = () => {
+  return (
+    <div>
+      <ul className="d-flex">
+        <li>Github</li>
+        <li>Linkedin</li>
+        <li>Email</li>
+        <li>CV</li>
+      </ul>
+    </div>
+  );
+};
+export default Social;
--- src/template/Header.jsx
+++ src/template/Header.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./Navbar";
+import Social from "../components/social/Social";
 
 const Header = () => {
   return (
     <>
       <header>
         <Navbar />
+        <Social />
       </header>
     </>
   );
