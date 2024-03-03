1e4dfef188d6ef09bb91f781c548b73c8e8cd82a
rober-web <robp.medeiros@gmail.com>
2024-03-03 22:59:14 +0000

    added button to check work and updated some styles

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,18 +1,32 @@
+import { Link } from "react-router-dom";
+
 const About = () => {
   return (
     <section
       id="about"
       className="min-vh-100 p-3 d-flex  align-items-center justify-content-center animate__animated animate__fadeInRight"
     >
-      <div className="text-center gap-20">
-        <h1 className="main-section-title">About Me</h1>
-        <p className="main-section-text">
-          I am a passionate web developer dedicated to creating visually
-          stunning and seamlessly functional websites. With a keen eye for
-          design and a love for coding, I strive to bring your digital vision to
-          life. Let's collaborate to build a compelling online presence that
-          leaves a lasting impression.
-        </p>
+      {" "}
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="text-center gap-20">
+              <h1 className="main-section-title">About Me</h1>
+            </div>
+          </div>
+          <div className="col-md-6 py-3 text-center">
+            <p className="main-section-text text-center">
+              I am a passionate web developer dedicated to creating visually
+              stunning and seamlessly functional websites. With a keen eye for
+              design and a love for coding, I strive to bring your digital
+              vision to life. Let's collaborate to build a compelling online
+              presence that leaves a lasting impression.
+            </p>
+            <Link className="btn btn-warning" to="/gallery">
+              Check my Work
+            </Link>
+          </div>
+        </div>
       </div>
     </section>
   );
