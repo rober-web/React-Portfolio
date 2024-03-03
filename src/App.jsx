8f8cf21083fab1758c2aee55bd2e0544b6f82f0a
rober-web <robp.medeiros@gmail.com>
2024-03-03 22:40:48 +0000

    added style.css to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 //import { useState } from "react";
-import React from 'react';
-
+import React from "react";
+import "./assets/css/style.css";
 import Header from "./template/Header";
 import HomePage from "./pages/HomePage";
 import Footer from "./template/Footer";
