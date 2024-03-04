e0b6b4d2450346d8ef8e2f863a7c575bcb2739c0
rober-web <robp.medeiros@gmail.com>
2024-03-04 00:43:00 +0000

    added padding top to not conflict with social links

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,9 +1,11 @@
 const Gallery = () => {
   return (
     <>
-      <section id="gallery" className="animate__animated animate__fadeInRight">
+      <section
+        id="gallery"
+        className="pt-5 animate__animated animate__fadeInRight"
+      >
         <h1 className="main-section-title text-center">Gallery</h1>
-        
       </section>
     </>
   );
