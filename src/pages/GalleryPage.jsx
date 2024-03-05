7c7423118456b623f5dd996f7eba829f6164025c
rober-web <robp.medeiros@gmail.com>
2024-03-05 15:49:35 +0000

    edited gallery page to receive gallery cards

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -6,10 +6,24 @@ const GalleryPage = () => {
   return (
     <div>
       <Header />
-      <div className="min-vh-100 pt-3 animate__animated animate__fadeInLeft">
-        <Gallery />
-        <h1 className=" main-section-title text-center">Gallery</h1>
-      </div>
+      <section
+        id="gallery"
+        className="min-vh-100 p-3 pt-5 main-section-bg animate__animated animate__fadeInLeft"
+      >
+        <div className="row">
+          <div className="col d-flex flex-column justify-content-center align-items-center pb-5">
+            <h1 className="main-section-title text-center">Gallery</h1>
+            <p className="text-center main-section-text">
+              Explore my portfolio, showcasing some web development projects
+              reflecting my passion for creating engaging and functional
+              websites with front-end and back-end expertise.
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <Gallery />
+        </div>
+      </section>
       <Footer />
     </div>
   );
