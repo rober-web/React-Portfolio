368bb9dcb9947479d463f255c95c4309511c19bc
rober-web <robp.medeiros@gmail.com>
2024-03-06 15:08:36 +0000

    update

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -4,12 +4,7 @@ import Footer from "../template/Footer";
 import Gallery from "../components/gallery/Gallery";
 //import Project from "../assets/utils/galleryCards.json";
 
-const GalleryPage = ({
-  title,
-  deployedVersionLink,
-  githubRepositoryLink,
-  screenshotSrc,
-}) => {
+const GalleryPage = () => {
   const [projectsData, setProjectsData] = useState([]);
 
   useEffect(() => {
@@ -27,16 +22,19 @@ const GalleryPage = ({
         id="gallery"
         className="min-vh-100  p-3 pt-5 main-section-bg animate__animated animate__fadeIn"
       >
-        <div className="row">
-          <div className="col d-flex flex-column justify-content-center align-items-center pb-5">
-            <h1 className="main-section-title text-center">Gallery</h1>
-            <p className="text-center main-section-text">
-              Explore my portfolio, showcasing some web development projects
-              reflecting my passion for creating engaging and functional
-              websites with front-end and back-end expertise.
-            </p>
+        <div className="container">
+          <div className="row mt-5">
+            <div className="col d-flex flex-column justify-content-center align-items-center pb-5">
+              <h1 className="main-section-title text-center">Gallery</h1>
+              <p className="text-center main-section-text">
+                Explore my portfolio, showcasing some web development projects
+                reflecting my passion for creating engaging and functional
+                websites with front-end and back-end expertise.
+              </p>
+            </div>
           </div>
         </div>
+
         <div className="row gap-4 justify-content-center">
           {projectsData.map((project, index) => (
             <Gallery
