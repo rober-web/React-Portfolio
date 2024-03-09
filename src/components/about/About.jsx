34ab2cfe993f9087aaf7ada478f292a05aac09b5
rober-web <robp.medeiros@gmail.com>
2024-03-09 20:29:24 +0000

    adjusted styles

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,10 +12,10 @@ const About = () => {
         <div className="row">
           <div className="col-md-6 d-flex align-items-center justify-content-center">
             <div className="text-center gap-20">
-              <h1 className="main-section-title">About Me</h1>
+              <h1 className="main-section-title-about">About Me</h1>
             </div>
           </div>
-          <div className="col-md-6 text-center about-info p-5">
+          <div className="col-md-6 py-3 text-center about-info ">
             <p className="main-section-text text-center">
               I am a passionate web developer dedicated to creating visually
               stunning and seamlessly functional websites. With a keen eye for
