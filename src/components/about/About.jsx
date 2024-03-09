b4206b712720854abf6128d1650a0ef357bda3af
rober-web <robp.medeiros@gmail.com>
2024-03-09 20:05:20 +0000

    added class and style to about info, and a background color to about background

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
               <h1 className="main-section-title">About Me</h1>
             </div>
           </div>
-          <div className="col-md-6 py-3 text-center">
+          <div className="col-md-6 text-center about-info p-5">
             <p className="main-section-text text-center">
               I am a passionate web developer dedicated to creating visually
               stunning and seamlessly functional websites. With a keen eye for
